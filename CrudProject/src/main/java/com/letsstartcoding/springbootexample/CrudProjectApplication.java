c717f400eed49ef94ac4fae2dfbf61ea37352371
usha9221 <bhavyar9221@gmail.com>
2019-03-25 13:29:09 +0530

    crud commited

---
+++ CrudProject/src/main/java/com/letsstartcoding/springbootexample/CrudProjectApplication.java
@@ -0,0 +1,16 @@
+package com.letsstartcoding.springbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class CrudProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudProjectApplication.class, args);
+	}
+
+}
+
---
+++ CrudProject/src/main/java/com/letsstartcoding/springbootexample/dao/IStudentDAO.java
@@ -0,0 +1,16 @@
+package com.letsstartcoding.springbootexample.dao;
+
+import java.util.List;
+
+import com.letsstartcoding.springbootexample.model.Student;
+
+public interface IStudentDAO {
+	public void save(Student p);
+	public List<Student> getAllStudents();
+	public List<Student> getStudentsByPage(int pageid, int total);
+	public Student getStudentById(int id);
+	public int count() ;
+	public void update(Student p);
+	public void delete(int id);
+	public void delete();
+}
